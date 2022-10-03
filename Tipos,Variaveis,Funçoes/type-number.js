95d2c45e1ba52b5930ac57da74e819f9826e1342
thiago <thiagcordeirodantas@gmail.com>
2022-10-03 15:54:35 -0300

    Variaveis do Tipo Numero, PontoFlutuante, NaN -> Not a Number Nao é um Numero

---
+++ Tipos,Variaveis,Funçoes/type-number.js
@@ -0,0 +1,23 @@
+ // Tipo Number
+
+ const  MeuNumero = 18;
+ const PrimeiroNumero = 1;
+ const SegundoNumero = 2;
+
+ const Operacao = Math.round(PrimeiroNumero + SegundoNumero) * 10
+
+ // Ponto Flutuante
+
+ const numeroPontoFlutuante = 3.3;
+
+ const segundoPontoFlutuante = 0.5
+
+ const OperacaoFlutuante = Math.round(numeroPontoFlutuante / segundoPontoFlutuante)
+
+
+ // NaN -> Not a Number (Nao é um Numero)
+
+
+const alura = "Alura"
+
+console.log(alura / MeuNumero)
