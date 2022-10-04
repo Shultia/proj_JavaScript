dbfa2832cdfcc9f8d65c749e2d14c3361ece6b1b
thiago <thiagcordeirodantas@gmail.com>
2022-10-04 14:09:57 -0300

    Tipo String

---
+++ Tipos,Variaveis,Funçoes/type-string.js
@@ -0,0 +1,18 @@
+// Tipo Texto
+
+const texto1 =  "Ola Mundo";
+const texto2 = "Ola Mundo";
+const senha = "Thiago98";
+const stringDeNumero = "48 32 23";
+
+const citacao = " O meu Nome é ";
+const nome = "Thiago"
+
+ // Concatençao (+)
+
+    console.log(citacao + nome)
+
+ // Template string OU Template literal
+
+
+
