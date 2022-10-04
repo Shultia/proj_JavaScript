636a5407dc33e014914c0b789b5108409344b3b7
thiago <thiagcordeirodantas@gmail.com>
2022-10-04 14:36:48 -0300

    Tipo Boolean Verdadeiro ou Falso

---
+++ Tipos,Variaveis,Funçoes/type-boolean.js
@@ -0,0 +1,16 @@
+// Tipos Booleanos
+
+// True (VERDADEIRO)
+// False (FALSO)
+
+const PrimeiroNumero = 10;
+const SegundoNumero = 20;
+let  CadastroAtivado = false;
+
+//console.log(PrimeiroNumero != SegundoNumero)
+
+
+const texto1 = "Alura";
+const texto2 = "alura"
+
+console.log(texto1 === texto2)
