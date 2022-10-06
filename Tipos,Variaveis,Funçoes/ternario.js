8f686e5f9d8697e5c9bb926b0227630e3ba34628
thiago <thiagcordeirodantas@gmail.com>
2022-10-06 17:46:01 -0300

    Operadores Ternarios

---
+++ Tipos,Variaveis,Funçoes/ternario.js
@@ -0,0 +1,15 @@
+    // Operador Ternario
+
+const idadeMininima = 18;
+let idade = 20;
+
+
+
+// if(idade >= idadeMininima) {
+//     console.log("Voce pode Beber")
+// }  else {
+//     console.log("Voce nao pode beber")
+// }
+
+                // Condicao             // true               //false
+console.log((idade >= idadeMininima ? "vc pode beber" : "vc nao pode beber"))
