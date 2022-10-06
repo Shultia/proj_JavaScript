379ac6c10a7a7eecd236143b6db2fce142a8b3df
thiago <thiagcordeirodantas@gmail.com>
2022-10-06 16:44:29 -0300

    Tipos Null e Undefined

---
+++ Tipos,Variaveis,Funçoes/truthy,falsy.js
@@ -0,0 +1,25 @@
+// boolean
+
+const usuarioLogado = true;
+const contaPaga = false;    
+
+// truthy e falsy
+
+// 0 => false
+// 1 => true
+
+// console.log(0 == false)
+// console.log("" == false)
+// console.log(1 == true)
+
+//null => Valor Nulo
+//undefined = Valor indefinido
+ let minhaVar;
+ let varNula = null; 
+ let numero = 3;
+ let texto = "alura";
+
+ console.log(typeof minhaVar)
+ console.log(typeof varNula)
+
+
