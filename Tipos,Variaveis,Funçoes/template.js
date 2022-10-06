933278bac11a8f70da1c9b4ed916ee5ff0f2acb5
thiago <thiagcordeirodantas@gmail.com>
2022-10-06 18:02:02 -0300

    template string

---
+++ Tipos,Variaveis,Funçoes/template.js
@@ -0,0 +1,6 @@
+const nome = "Thiago"
+const idade = 2022-2004;
+const cidade = "SP"
+const apresentacao = `me chamo ${nome},  tenho ${idade} anos, e moro em ${cidade} `
+
+console.log(apresentacao)
