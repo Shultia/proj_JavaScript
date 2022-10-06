f274b8f632d4e1fcdd501006a009b01ad72058b3
thiago <thiagcordeirodantas@gmail.com>
2022-10-06 17:04:25 -0300

    Conversao de Valores

---
+++ Tipos,Variaveis,Funçoes/conversao.js
@@ -0,0 +1,16 @@
+// Tipos de Dados
+// Booleanos
+
+// Conversao implicita
+
+    const numero = 10;
+    const numeroString = Number("1s0");
+
+
+// Conversao explicita
+
+//  Number()   
+//  String()
+
+    console.log(numero + numeroString);
+
