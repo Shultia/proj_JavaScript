d939be2b8af40b50a9117e28dfcb322b84f35775
thiago <thiagcordeirodantas@gmail.com>
2022-10-06 16:28:58 -0300

    Tipos de Declaraçao de Variavel const let var

---
+++ Tipos,Variaveis,Funçoes/script.js
@@ -0,0 +1,41 @@
+// var
+
+// var altura = 5;
+// var comprimento = 7;
+
+//  var area = altura * comprimento;
+
+// console.log(area)
+
+
+// let
+
+// let forma = "triangulo";
+// let altura = 10;
+// let comprimento = 7;
+// let area;
+
+// if(forma === "retangulo" ){
+//     area = altura * comprimento
+// } else {
+//     area = altura * comprimento / 2;
+// }
+
+// console.log(area)
+
+
+// Const
+
+const forma = "triangulo"
+const altura = 10;
+const comprimento = 30;
+let area;
+
+
+if(forma === "quadrado"){
+   area = altura * comprimento
+} else {
+    area = altura * comprimento / 2;
+}
+
+console.log(area)
