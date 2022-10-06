30df9af73ceb07f16b1a58599658a720d3a30f1a
thiago <thiagcordeirodantas@gmail.com>
2022-10-06 17:16:57 -0300

    Comparacoes de Valores

---
+++ Tipos,Variaveis,Funçoes/comparacoes.js
@@ -0,0 +1,20 @@
+    // ==  (Comparacao implicita)
+
+    const numero = 200;
+    const texto = "200";
+
+    console.log(numero == texto);
+
+    // Typeof
+
+    console.log(typeof numero);
+    console.log(typeof texto);
+
+    // == (Compara apenas o valor)
+    // === (Compara o valor e o tipo do dado)
+
+
+    // Conversao Explicita
+
+    Number();
+    String();
