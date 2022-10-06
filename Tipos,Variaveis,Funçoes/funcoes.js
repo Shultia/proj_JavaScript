e751df49c81d06661aece1ed15b63f4c4201f507
thiago <thiagcordeirodantas@gmail.com>
2022-10-06 18:37:01 -0300

    Funcoes

---
+++ Tipos,Variaveis,Funçoes/funcoes.js
@@ -0,0 +1,29 @@
+// let x = "";
+
+// console.log(x)
+// x = "oi"
+
+
+// DECLARACAO DE FUNCAO 
+
+// Primeiro momento declarar 
+
+            //string
+function x (texto){
+    console.log(texto)
+}
+
+//Segundo momento executar funcao
+
+x("oi");
+
+// tres formas de escrever uma funcao
+
+function soma() {
+    return  (20 + 30 + 40) / 3
+   
+}
+
+
+console.log(soma());
+
