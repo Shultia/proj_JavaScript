1023a41e80c54c7515dc55f468ec9b721aec69ab
thiago <thiagcordeirodantas@gmail.com>
2022-10-09 16:14:00 -0300

    parametros em funçoes

---
+++ Tipos,Variaveis,Funçoes/parametros.js
@@ -0,0 +1,46 @@
+// parametros de funcao
+
+
+
+
+// function somaOutroNumero() {    
+//     return 6 * 4 / 3
+// }
+
+// console.log(soma(2,21))
+// console.log(soma(222,221))
+// console.log(soma(23,221))
+// console.log(somaOutroNumero())
+
+// parametros x argumentos
+
+// ordem dos parametros
+
+
+// function nomeIdade (nome,idade){
+//     return `meu nome é  ${nome} e eu tenho ${idade} anos`
+// }
+
+
+// console.log(nomeIdade("Jose", 18))
+
+function soma (num1 , num2){
+    return num1 + num2 ;
+}
+
+
+function multiplica (numero1,numero2 = 2){
+    return numero1 * numero2 
+}
+                            //9           //6
+console.log(multiplica(soma(4,5)))
+
+
+
+
+
+
+
+
+
+
