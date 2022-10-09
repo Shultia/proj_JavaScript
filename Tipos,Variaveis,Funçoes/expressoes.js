457510df9cdf44817802faf5d700330795567188
thiago <thiagcordeirodantas@gmail.com>
2022-10-09 16:29:30 -0300

    expressoes

---
+++ Tipos,Variaveis,Funçoes/expressoes.js
@@ -0,0 +1,27 @@
+// Forma declarativa
+
+function MinhaFuncao(param){
+    //bloco de codigo
+}
+
+// MinhaFuncao("param");
+
+// // expressao de funçao
+
+const soma = function (num1,num2) {
+    return num1 + num2
+}
+
+// console.log(soma(2,2))
+
+// Diferença Principal ??????
+
+
+
+function apresentar(){
+    return "oiii"
+}
+
+console.log(apresentar());
+
+
