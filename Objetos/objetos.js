3ae69fadaba2d6aab7a6fb9247a70bdb23e015d6
thiago <thiagcordeirodantas@gmail.com>
2022-10-10 21:15:10 -0300

    first commit

---
+++ Objetos/objetos.js
@@ -0,0 +1,10 @@
+const listaCPFs = ["1232323322323","233232144676",9120912272]
+
+//const cliente = ["nome","Thiago","Idade",36]
+
+const cliente = {
+    nome:"Thiago",
+    idade: 36,
+    cpf : "343433232",
+    email: "thiago@gmail.com"
+}
