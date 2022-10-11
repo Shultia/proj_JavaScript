0fed77900b2ad49193e77c10dcbaec8459cf57ac
thiago <thiagcordeirodantas@gmail.com>
2022-10-11 12:35:36 -0300

    Acessando dados

---
+++ Objetos/print-info.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome:"Thiago",
+    idade: 18,
+    cpf : "343433232",
+    email: "thiago@gmail.com"
+}
+
+console.log(`Eu me chamo ${cliente.nome}, tenho ${cliente.idade} anos`)
+console.log(cliente.cpf.substring(0,3))
