2aec305f0b3a9feb8a2b2b58e5171e70338dbab9
Thiago Cordeiro <thiagcordeirodantas@gmail.com>
2022-10-17 18:22:46 -0300

    Acessando telefone

---
+++ Objetos/acessarTelefone.js
@@ -0,0 +1,10 @@
+const cliente = {
+    nome : "Thiago",
+    idade: 36,
+    cpf: "12232234",
+    email: "cordeiro@gmail.com",
+    fones: ["11964215535","119087432"]
+}
+
+
+console.log(cliente.fones.forEach(fone => console.log(fone)))
