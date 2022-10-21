516fdc30b2b63b5fd2527846c290746ae4ada0da
Thiago Cordeiro <thiagcordeirodantas@gmail.com>
2022-10-20 22:57:42 -0300

    Manipulando prototype

---
+++ Objetos/cliente-poupanca.js
@@ -0,0 +1,26 @@
+function Cliente (nome,cpf,email,saldo){
+    this.nome = nome 
+    this.cpf = cpf
+    this.email = email
+    this.saldo = saldo
+    this.depositar = function(valor){
+    this.saldo += valor
+    }
+}
+
+function clientePoupanca(nome,cpf,email,saldo,saldoP){
+    Cliente.call(this,nome,cpf,email,saldo)
+    this.saldoP = saldoP
+}
+
+
+
+const thiago = new clientePoupanca("thiago","48302072323","thiago@gmail.com",130,400)
+
+clientePoupanca.prototype.depositarP = function(valor){
+    this.saldoP += valor
+}
+
+thiago.depositarP(400)
+
+console.log(thiago)
