fa666f0741b70217f96e56340b4704c266f09549
Thiago Cordeiro <thiagcordeirodantas@gmail.com>
2022-10-20 23:06:49 -0300

    Criando uma Classe

--- Objetos/classe-cliente.js
+++ Objetos/classe-cliente.js
@@ -1,3 +1,20 @@
 class Cliente {
-    
+    constructor(nome,email,cpf,saldo){
+        this.nome = nome
+        this.email = email
+        this.cpf = cpf
+        this.saldo = saldo
+    }
+
+    depositar(valor){
+        this.saldo += valor
+    }
+
+    exibirSaldo(){
+        console.log(this.saldo)
+    }
 }
+
+const thiago = new Cliente ("thiago","thiago@gmail.com","4833222232",400)
+
+console.log(thiago)
