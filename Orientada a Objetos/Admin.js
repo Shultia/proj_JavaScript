0935c3218e8275ba659be4b860a2a6fde532a75c
Thiago Cordeiro <thiagcordeirodantas@gmail.com>
2022-10-24 14:04:11 -0300

    Criado classe admin

--- Orientada a Objetos/Admin.js
+++ Orientada a Objetos/Admin.js
@@ -4,10 +4,10 @@ class Admin extends User {
     constructor( nome, email, nascimento, role ='Admin', ativo = true) {
         super(nome,email,nascimento,role,ativo)
     }
+    criarCurso(nomeDoCurso,vagas){
+        return `Curso de ${nomeDoCurso} criado com ${vagas} Vagas`
+    }
 }
 
 const novoAdmin = new Admin('thiagos','1@gmail.com','20-02-2000')
-
-
-console.log(novoAdmin)
-console.log(novoAdmin.exibirInfos())
+console.log(novoAdmin.criarCurso('js','20'))
