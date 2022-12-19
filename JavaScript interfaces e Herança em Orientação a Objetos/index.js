affcf577f924ed22ed787a679f40b718782c081c
Thiago Cordeiro <thiiagocordeirodantassz@gmail.com>
2022-12-19 15:30:14 -0300

    Verificando propriedades do sistema de autenticacao

--- JavaScript interfaces e Herança em Orientação a Objetos/index.js
+++ JavaScript interfaces e Herança em Orientação a Objetos/index.js
@@ -5,12 +5,16 @@ import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
 
 const diretor = new Diretor("Thiago",5000,32023030230);
-
 const gerente = new Gerente("Joaquim", 2500,30200320231);
+const cliente = new Cliente("Lais",323,230320230);
 
-gerente.cadastrarSenha("123")
-diretor.cadastrarSenha("1234")
-const logado = SistemaAutenticacao.login(gerente,"123");
+diretor.cadastrarSenha("123456");
+gerente.cadastrarSenha("123456")
 
 
-console.log(logado);
+
+ const DiretorLogin = SistemaAutenticacao.login(diretor,"123456");
+ const GerenteLogin = SistemaAutenticacao.login(gerente,"123456");
+ const clienteLogin = SistemaAutenticacao.login(cliente,"123456")
+
+console.log(DiretorLogin,GerenteLogin,clienteLogin);
