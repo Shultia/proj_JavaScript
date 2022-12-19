68db1ac2687f83b8022fc08c3c49d785b5de832e
Thiago Cordeiro <thiiagocordeirodantassz@gmail.com>
2022-12-19 14:56:36 -0300

    Polimorfismo

---
+++ JavaScript interfaces e Herança em Orientação a Objetos/SistemaAutenticacao.js
@@ -0,0 +1,7 @@
+export class SistemaAutenticacao {
+    static login(funcionario,senha){
+       funcionario.senha == senha
+         return funcionario.senha == senha;
+        
+    }
+}
--- JavaScript interfaces e Herança em Orientação a Objetos/index.js
+++ JavaScript interfaces e Herança em Orientação a Objetos/index.js
@@ -1,17 +1,16 @@
-import {Cliente} from "./Contas/Cliente.js";
-import {ContaCorrente} from "./Contas/ContaCorrente.js";
-import {ContaPoupanca} from "./Contas/ContaPoupanca.js";
-import { ContaSalario } from "./Contas/ContaSalario.js";
+import {Cliente} from "./Conta/Cliente.js";
+import { Gerente } from "./Funcionarios/Gerente.js";
+import { Diretor } from "./Funcionarios/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
 
-const cliente1 = new Cliente("Thiago",500029088730);
+const diretor = new Diretor("Thiago",5000,32023030230);
+
+const gerente = new Gerente("Joaquim", 2500,30200320231);
+
+gerente.cadastrarSenha("123")
+diretor.cadastrarSenha("1234")
+const logado = SistemaAutenticacao.login(gerente,"123");
 
 
-const ContaCorrenteThiago = new ContaCorrente(0,cliente1,1001);
-const contaPoupanca = new ContaPoupanca(50,cliente1,1001);
-
-const contaSalario = new ContaSalario(cliente1);
-contaSalario.depositar(100);
-contaSalario.sacar(50);
-
-console.log(contaSalario);
+console.log(logado);
