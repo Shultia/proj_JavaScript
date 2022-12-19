e9bb3eeec442efa2e1076a0778934e9460310e82
Thiago Cordeiro <thiiagocordeirodantassz@gmail.com>
2022-12-18 22:34:30 -0300

    Heranca
    compartilhar código de uma maneira um pouco mais legal, em que não perdemos o vocabulário, continuamos tendo uma linguagem de negócio mais próxima da linguagem de negócio, conta-corrente e conta poupança, que são os termos que o ByteBank vai usar, ao mesmo tempo em que estamos compartilhando código entre essas duas classes, fazendo a herança delas.

--- JavaScript interfaces e Herança em Orientação a Objetos/Cliente.js
+++ JavaScript interfaces e Herança em Orientação a Objetos/Cliente.js
@@ -2,7 +2,7 @@ export class Cliente {
     get cpf(){
         return this._cpf;
     }
-
+    
     constructor(nome, cpf){
         this.nome = nome;
         this._cpf = cpf;
--- JavaScript interfaces e Herança em Orientação a Objetos/index.js
+++ JavaScript interfaces e Herança em Orientação a Objetos/index.js
@@ -2,14 +2,15 @@ import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
 import {ContaPoupanca} from "./ContaPoupanca.js";
 
+
 const cliente1 = new Cliente("Thiago",500029088730);
 
 
-const ContaCorrenteThiago = new ContaCorrente(1001,cliente1);
+const ContaCorrenteThiago = new ContaCorrente(0,cliente1,1001);
 ContaCorrenteThiago.depositar(355);
 ContaCorrenteThiago.sacar(55);
 
 const contaPoupanca = new ContaPoupanca(50,cliente1,1001);
 
 console.log(contaPoupanca);
-console.log(ContaCorrenteThiago);
+console.log(ContaCorrenteThiago); 
