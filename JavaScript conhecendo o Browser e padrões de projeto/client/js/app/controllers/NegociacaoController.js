938cce7bc9c1fc904f1675fb87494a5b6fa1ee8a
Thiago Cordeiro <thiiagocordeirodantassz@gmail.com>
2022-12-22 16:57:15 -0300

    Associando metodos de controller as acoes do usuario

--- JavaScript conhecendo o Browser e padrões de projeto/client/js/app/controllers/NegociacaoController.js
+++ JavaScript conhecendo o Browser e padrões de projeto/client/js/app/controllers/NegociacaoController.js
@@ -2,6 +2,16 @@ class NegociacaoController {
 
     adiciona(event){
         event.preventDefault();
-        alert('Chamei acao Controller');
+       
+
+        let $ = document.querySelector.bind(document);
+        let inputData = $('#data');
+        let inputQuantidade = $('#quantidade');
+        let inputValor = $('#valor');
+
+
+        console.log(inputData.value);
+        console.log(inputQuantidade.value);
+        console.log(inputValor.value)
     }
 }
