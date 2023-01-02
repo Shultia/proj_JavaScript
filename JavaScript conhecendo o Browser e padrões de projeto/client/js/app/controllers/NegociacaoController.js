3b52a53bb3004a0a4a4a666068db1f31f55230fd
Thiago <thiagocordeirodantaszx@gmail.com>
2023-01-02 01:21:12 -0300

    Criando classe mensagemView

--- JavaScript conhecendo o Browser e padrões de projeto/client/js/app/controllers/NegociacaoController.js
+++ JavaScript conhecendo o Browser e padrões de projeto/client/js/app/controllers/NegociacaoController.js
@@ -8,12 +8,17 @@ class NegociacaoController {
         this._listaNegociacoes = new ListaNegociacoes();
         this._negociacoesView = new NegociacaoView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
     }
     adiciona(event){
         event.preventDefault();
 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem.texto = "Negociaçao adicionada com Sucesso"
+        this._mensagemView.update(this._mensagem);
+        this._negociacoesView.update(this._listaNegociacoes);   
         this._limpaFormulario();
     }
 
