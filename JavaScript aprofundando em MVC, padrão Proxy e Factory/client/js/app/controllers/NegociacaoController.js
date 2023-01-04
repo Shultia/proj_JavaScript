637015eed5ddfc630ce11a3edad9806a1c0fc274
Thiago <thiagocordeirodantaszx@gmail.com>
2023-01-03 22:23:16 -0300

    construindo armadilhas para metodos

--- JavaScript aprofundando em MVC, padrão Proxy e Factory/client/js/app/controllers/NegociacaoController.js
+++ JavaScript aprofundando em MVC, padrão Proxy e Factory/client/js/app/controllers/NegociacaoController.js
@@ -5,51 +5,51 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-
+        
         /*
         this._listaNegociacoes = new ListaNegociacoes(model  => 
-        this._negociacoesView.update(model));
-        */
-       
-        this._negociacoesView = new NegociacaoView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
-
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
+            this._negociacoesView.update(model));
+            */
+            
+            this._negociacoesView = new NegociacaoView($('#negociacoesView'));
+            this._negociacoesView.update(this._listaNegociacoes);
+            
+            this._mensagem = new Mensagem();
+            this._mensagemView = new MensagemView($('#mensagemView'));
+            this._mensagemView.update(this._mensagem);
+            
+        }
+        adiciona(event){
+            event.preventDefault();
+            
+            this._listaNegociacoes.adiciona(this._criaNegociacao());
+            this._mensagem.texto = "Negociaçao adicionada com Sucesso"
+            this._mensagemView.update(this._mensagem);
+            this._limpaFormulario();
+        }
         
-    }
-    adiciona(event){
-        event.preventDefault();
-
-        this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._mensagem.texto = "Negociaçao adicionada com Sucesso"
-        this._mensagemView.update(this._mensagem);
-        this._limpaFormulario();
-    }
-
-    apaga(){
-
-        this._listaNegociacoes.esvazia();
-       
-        this._mensagem.texto = 'Negociaçoes apagadas com sucesso';
-        this._mensagemView.update(this._mensagem);
-    }
-
-    _criaNegociacao(){
-        return new Negociacao(
-            DateHelper.textoParaData(this._inputData.value),
-            this._inputQuantidade.value,
-            this._inputValor.value);
-    }
-
-    _limpaFormulario(){
-        this._inputData.value = '';
-        this._inputQuantidade.value = '1';
-        this._inputValor.value = '0.0';
-
-        this._inputData.focus();
-    }
-}   
+        apaga(){
+            
+            this._listaNegociacoes.esvazia();
+            
+            this._mensagem.texto = 'Negociaçoes apagadas com sucesso';
+            this._mensagemView.update(this._mensagem);
+        }
+        
+        _criaNegociacao(){
+            return new Negociacao(
+                DateHelper.textoParaData(this._inputData.value),
+                this._inputQuantidade.value,
+                this._inputValor.value);
+            }
+            
+            _limpaFormulario(){
+                this._inputData.value = '';
+                this._inputQuantidade.value = '1';
+                this._inputValor.value = '0.0';
+                
+                this._inputData.focus();
+            }
+        }   
         
         
